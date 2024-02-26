167afa29997fbc252b34a929c68fe7fae37561fc
kieronk <kieronkirkland@gmail.com>
2024-02-26 10:33:58 +0000

    refactors first version

--- milestone_2.py
+++ milestone_2.py
@@ -1,21 +1,16 @@
 import random
 
-word_list = ["apple", "blueberry", "strawberry", "raspberry", "pineapple"]
+fruit_list = ["apple", "blueberry", "strawberry", "raspberry", "pineapple"]
 
-word = random.choice(word_list)
+random_word = random.choice(fruit_list)
 
-guess = input('Enter a single letter:')
+user_guess = input('Enter a single letter:')
 
-if len(guess) == 1 and guess.isalpha() == True:
+if len(user_guess) == 1 and user_guess.isalpha() == True:
     print('Good guess!')
 else:  
     print("Oops! That's not a valid input.") 
- 
-#if len(guess) == 1 and guess.isalpha() == True:
-#    print('Good guess!')
-#else:  
-#    "Oops! That's not a valid input."
 
-print(word_list)
+#print(word_list)
+#print(word)
 
-print(word)
